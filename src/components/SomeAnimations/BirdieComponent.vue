996d6c79bf5c8eaeb28cc9725e23e21c2d00fd74
Casley-Owl <owldoitlater@gmail.com>
2023-05-16 21:55:34 -0400

    can use linear easing and many images to zoom in ! very nice effect

--- src/components/SomeAnimations/BirdieComponent.vue
+++ src/components/SomeAnimations/BirdieComponent.vue
@@ -3,30 +3,28 @@
 	import { ScrollTrigger } from "gsap/ScrollTrigger";
 	import { onMounted } from "vue";
 
-	onMounted(() => {
-		gsap.set("img", {
-			yPercent: -50,
-			xPercent: -50,
-			top: "50%",
-			left: "50%",
-			minWidth: "100%",
-			position: "absolute",
-		});
-
-		//linear easing doesn't APPEAR linear (visually).
-		gsap.fromTo(
-			"#linear img",
-			{ scale: 0.1 },
-			{ scale: 8, duration: 10, ease: "none", repeat: -1 }
-		);
-
-		//notice how we feed in the starting and ending [scale] values to "expoScale()" so that it can adjust its curve accordingly so that scaling appears linear visually even though technically the scale values aren't animating linearly at all.
-		gsap.fromTo(
-			"#expo img",
-			{ scale: 0.1 },
-			{ scale: 8, duration: 10, ease: "expoScale(0.1, 8)", repeat: -1 }
-		);
+	gsap.set("img", {
+		yPercent: -50,
+		xPercent: -50,
+		top: "50%",
+		left: "50%",
+		minWidth: "100%",
+		position: "absolute",
 	});
+
+	//linear easing doesn't APPEAR linear (visually).
+	gsap.fromTo(
+		"#linear img",
+		{ scale: 0.1 },
+		{ scale: 8, duration: 10, ease: "none", repeat: -1 }
+	);
+
+	//notice how we feed in the starting and ending [scale] values to "expoScale()" so that it can adjust its curve accordingly so that scaling appears linear visually even though technically the scale values aren't animating linearly at all.
+	gsap.fromTo(
+		"#expo img",
+		{ scale: 0.1 },
+		{ scale: 8, duration: 10, ease: "expoScale(0.1, 8)", repeat: -1 }
+	);
 
 	//for a more complicated example that shows multi-step zooming
 	//(swapping new images for better resolution as it progresses),
@@ -35,7 +33,7 @@
 
 <template>
 	<div id="container">
-		<!-- <div id="expo">
+		<div id="expo">
 			<img
 				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-1.jpg"
 			/>
@@ -103,25 +101,75 @@
 				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-22.jpg"
 			/>
 			<div class="label">ExpoScaleEase</div>
-		</div> -->
+		</div>
 
 		<div id="linear">
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-			<img src="../assets/images/clearturnroad.jpg" />
-
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-1.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-2.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-3.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-4.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-5.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-6.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-7.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-8.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-9.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-10.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-11.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-12.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-13.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-14.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-15.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-16.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-17.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-18.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-19.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-20.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-21.jpg"
+			/>
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/zoom-22.jpg"
+			/>
 			<div class="label">Linear easing</div>
 		</div>
 
@@ -139,7 +187,9 @@
 		padding: 0;
 		background-color: black;
 	}
-
+	#expo {
+		/*display: none;*/
+	}
 	#container {
 		display: flex;
 		min-height: 100vh;
